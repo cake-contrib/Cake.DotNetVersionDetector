5ce20920110d150a0f46cce24768f0db9e5a8e0e
GitHub <noreply@github.com>
2024-07-21 20:23:36 +0000

    Continuous Integration Publish: 5c9982eaed2327d8cc2be4d429253da1527b1ad3 Merge pull request #135 from nils-a/release/1.1.0
    Release/1.1.0

--- assets/js/searchIndex.js
+++ assets/js/searchIndex.js
@@ -39,20 +39,6 @@ var searchModule = function() {
     a(
         {
             id:0,
-            title:"DotNetVersionDetectorRunner",
-            content:"DotNetVersionDetectorRunner",
-            description:'',
-            tags:''
-        },
-        {
-            url:'/Cake.DotNetVersionDetector/api/Cake.DotNetVersionDetector/DotNetVersionDetectorRunner',
-            title:"DotNetVersionDetectorRunner",
-            description:""
-        }
-    );
-    a(
-        {
-            id:1,
             title:"DotNetVersionDetectorAliases",
             content:"DotNetVersionDetectorAliases",
             description:'',
@@ -66,7 +52,7 @@ var searchModule = function() {
     );
     a(
         {
-            id:2,
+            id:1,
             title:"DotNetVersionDetectorSettings",
             content:"DotNetVersionDetectorSettings",
             description:'',
@@ -75,6 +61,20 @@ var searchModule = function() {
         {
             url:'/Cake.DotNetVersionDetector/api/Cake.DotNetVersionDetector/DotNetVersionDetectorSettings',
             title:"DotNetVersionDetectorSettings",
+            description:""
+        }
+    );
+    a(
+        {
+            id:2,
+            title:"DotNetVersionDetectorRunner",
+            content:"DotNetVersionDetectorRunner",
+            description:'',
+            tags:''
+        },
+        {
+            url:'/Cake.DotNetVersionDetector/api/Cake.DotNetVersionDetector/DotNetVersionDetectorRunner',
+            title:"DotNetVersionDetectorRunner",
             description:""
         }
     );
